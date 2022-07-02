e819303b63d3b8d848d957546a4c60dd7139fa93
WannaBAcoder <vgmcn3@mst.edu>
2022-07-02 01:39:20 -0500

    Update NewRemote.ino
    Added improved UI to controller for clearly mapping buttons/actions

--- Arduino/NewRemote/NewRemote.ino
+++ Arduino/NewRemote/NewRemote.ino
@@ -15,14 +15,16 @@ Adafruit_RGBLCDShield lcd = Adafruit_RGBLCDShield();
 
 //Variables for menu
 int column = 0;
-int function = 0;
+int function = 10;
 int buttonValue = 0;
 
-int buttons[] = {2, 3, 4, 7, 8, 9, 10, 11, 12, 13};
+int buttons[] = {2, 3, 4, 7, 8, 9, 11, 10, 13, 12};
 
 int text_length = 0;
 enum states{interlock, down, leg3, leg2, up};
 
+unsigned long clear_ch5_time = 0, clear_ch6_time = 0;
+bool clear_ch5 = 0, clear_ch6 = 0;
 
 void setup() 
 {
@@ -32,8 +34,9 @@ void setup()
   lcd.print("R6 Droid Control");
   delay(2000);
   lcd.clear();
-  lcd.print("Actions         ");
   lcd.setBacklight(GREEN);
+  lcd.print("Actions         ");
+  print_screen(function/10); 
   
   pinMode(5, OUTPUT); // channel 5
   pinMode(6, OUTPUT); // channel 6
@@ -41,8 +44,6 @@ void setup()
   for(int i = 0; i < 10; i++)
     pinMode(buttons[i], INPUT_PULLUP);
 }
-
-
 
 void loop() 
 {
@@ -52,33 +53,40 @@ void loop()
   for(int i = 0; i < 10; i++)
   {
     if(!digitalRead(buttons[i]))
-      buttonValue = i*10+10;//assign button value 
+    {
+      buttonValue = i*1+1;//assign button value 
+      break;
+    }
   }
   
-  //if (buttonValue!=0)
-  //{
-    analogWrite(6, buttonValue);//write button value PWM
-    lcd.setCursor(13,0);
+  if(buttonValue != 0)//check if we had a button press
+  {
+    if(buttonValue == 10)//handle text shifting between signle and double digit
+      lcd.setCursor(14,0);
+    else
+      lcd.setCursor(15,0);
+      
     lcd.print(buttonValue);
-    lcd.print("   ");
-//    delay(500);
-//    analogWrite(6, 0);
-//    lcd.setCursor(13,0);
-//    lcd.print("0");
-//    lcd.print("    ");
-  //}
-  
+ 
+    analogWrite(6, buttonValue*10);//write button value PWM
+    clear_ch6 = 1;
+    clear_ch6_time = millis();
+
+    delay(1000);
+  }
+
+
   uint8_t LCD_buttons = lcd.readButtons();//read LCD buttons
 
-  if (LCD_buttons) 
+  if (LCD_buttons) //handle the LCD buttons
   {
     if (LCD_buttons & BUTTON_LEFT) 
     {
       lcd.setCursor(0,0);
       lcd.print("Actions         ");
       lcd.setBacklight(GREEN);
-      column = column - 130;
-      column = constrain(column,0,130);   
+      column = column - 13;
+      column = constrain(column,0,13);   
 
       print_screen(function);
     }
@@ -87,8 +95,8 @@ void loop()
       lcd.setCursor(0,0);
       lcd.print("Sounds           ");
       lcd.setBacklight(RED);
-      column = column + 130;
-      column = constrain(column,0,130);
+      column = column + 13;
+      column = constrain(column,0,13);
       
       lcd.setCursor(0,1);
       lcd.print(function);
@@ -96,70 +104,112 @@ void loop()
     }
     if (LCD_buttons & BUTTON_DOWN) 
     {
-      function = function - 10;   
-      print_screen(function);
+      if(function - 1 != 0)
+      {
+        function = function - 1;   
+        print_screen(function);
+      }
     }
     if (LCD_buttons & BUTTON_UP) 
     {
-      function = function + 10;
+      function = function + 1;
       print_screen(function); 
     }
 
     
     if (LCD_buttons & BUTTON_SELECT) 
     {
-      lcd.setCursor(text_length,1);
+      if(column == 13)//sounds only called numbers
+      {
+        if(function >= 10)
+          lcd.setCursor(14,1);
+        else
+          lcd.setCursor(15,1);
+            
+        lcd.print(function);
+      }
 
-      //handle state machine here to bind 2-3-2 actions together
+      else//actions can be displayed as text
+      {
+        lcd.setCursor(text_length,1);
+  
+        //generate remaning spaces to fill rest of line after action text
+        String spaces;
+        int num_spaces = 15;
+        if(function >= 10)
+          num_spaces = 14;
+          
+        for(int i = 0; i < num_spaces-text_length; i++)
+          spaces+= " ";
+          
+        lcd.print(spaces);
+        lcd.print(function);
+      }
+      
+      analogWrite(5, (column*10)+(function*10));
+      clear_ch5 = 1;
+      clear_ch5_time = millis();
+    }
+  }
 
-      //generate remaning spaces to fill rest of line after action text
-      String spaces;
-      for(int i = 0; i < 13-text_length; i++)
-        spaces+= " ";
-        
-      lcd.print(spaces);
-      lcd.print(column+function);
-     
-      analogWrite(5, column+function);
-      delay(500);
+  if(clear_ch5 || clear_ch6)
+  {
+    //shut off command after period of time
+    if(millis() - clear_ch5_time >= 1000)
+    {
       analogWrite(5, 0);
+  
+      lcd.setCursor(13,1);
+      lcd.print("   ");
+      clear_ch5 = 0;
+    }
+  
+    //shut off command after period of time
+    if(millis() - clear_ch6_time >= 1000)
+    {
+      analogWrite(6, 0);
+  
+      lcd.setCursor(13,0);
+      lcd.print("   ");
+      clear_ch6 = 0;
     }
   }
 }
 
+
 //This function allows for custom labels for actions
 void print_screen(int func)
 {
-  function = constrain(function,0,120);
+  function = constrain(function,0,12);
   lcd.setCursor(0,1);
 
-  if(column == 130)
+  if(column == 13)
   {
     lcd.print(func);
-    lcd.print("             ");
+    lcd.print("              ");
   }
   else
   {
     //can add as many names as you want here, be sure to include text length <13 chars
     switch(func)
     {
-      case 10:
+      case 1:
         lcd.print("INTERLOCK       ");
         text_length = 9;
       break;
-      case 20:
+      case 2:
         lcd.print("LEG DOWN        ");
         text_length = 8;
       break;
-      case 30:
+      case 3:
         lcd.print("3 LEG MODE      ");
         text_length = 10;
       break;
-      case 40:
+      case 4:
         lcd.print("2 LEG MODE      ");
         text_length = 10;
       break;
-      case 50:
+      case 5:
         lcd.print("LEG UP          ");
         text_length = 6;
       break;
